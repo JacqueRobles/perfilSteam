7d50b0c76ffef53e7b48d030310e7f31f1f3e74b
JacqueRobles <116358744+JacqueRobles@users.noreply.github.com>
2022-11-22 17:01:09 -0300

    Update mainSteam.py

--- mainSteam.py
+++ mainSteam.py
@@ -43,3 +43,52 @@ def main():
         
         
 main()
+
+
+
+
+
+
+from perfilSteam import Profile
+
+# def ccontraseña ():
+#     print("¿Desea cambiar la contraseña?")
+#     option =int(input("(1)Si    (2)No \n"))
+#     if (option == 1):
+#         password = str(input("Ingrese su contraseña: "))
+#         rePassword = str(input("Repita su contraseña: "))
+        
+#         profile1 = Profile(userName, id, email, password)
+#         profile1.print()
+def main():
+
+    
+        userName = str(input("Ingrese su nombre de usuario: "))    
+        id = input("Ingrese su id: ")
+        email = str(input("Ingrese su email: "))
+        while(True):
+            password = str(input("Ingrese su contraseña: "))
+            rePassword = str(input("Repita su contraseña: "))
+            
+            if (password != rePassword):
+                print("Las contraseñas no coinciden")
+                continue
+            else: break
+                
+        profile1 = Profile(userName, id, email, password)
+
+        profile1.print()
+        while(True):
+            print("¿Desea cambiar la contraseña?")
+            option =int(input("(1)Si    (2)No \n"))
+            if (option == 1):
+                password = str(input("Ingrese su contraseña: "))
+                rePassword = str(input("Repita su contraseña: "))
+                
+                profile1 = Profile(userName, id, email, password)
+                profile1.print()
+                
+            elif (option ==2):
+                break
+            
+main()
